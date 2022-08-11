d7a07fac5219d397ab8a043759845c2c732853f2
laneleka <4ebotaeva@gmail.com>
2022-08-11 13:32:24 +0300

    Added Homework for Lesson 5

---
+++ Lesson_5/script.js
@@ -0,0 +1,35 @@
+let userName = '';
+let userSurname = '';
+let userEmail = '';
+let userYearOfBirth = '';
+
+while (!userName) {
+  userName = prompt('What is your name?');
+  if (userName) userName = userName.trim();
+}
+
+while (!userSurname) {
+  userSurname = prompt('What is your surname?');
+  if (userSurname) userSurname = userSurname.trim();
+}
+
+while (!userEmail || !userEmail.includes('@') || userEmail.startsWith('@') || userEmail.endsWith('@')){
+  userEmail = prompt('What is your email?');
+  if (userEmail) userEmail = userEmail.replaceAll(' ', '').toLowerCase();
+}
+
+while (!userYearOfBirth) {
+  userYearOfBirth = prompt('Please, type the year of your birth');
+  if (userYearOfBirth) userYearOfBirth = parseInt(userYearOfBirth.replaceAll(' ', ''));
+}
+
+const age = new Date().getFullYear() - userYearOfBirth;
+
+document.write(`
+  <ul>
+    <li>Full name: ${userName} ${userSurname}</li>
+    <li>Email: ${userEmail}</li>
+    <li>Age: ${age}</li>
+  </ul>
+`);
+
