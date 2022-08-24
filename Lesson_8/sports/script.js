b4d4641a0ba569760fbc1b4f17c33826928d3dc6
laneleka <4ebotaeva@gmail.com>
2022-08-24 14:01:01 +0300

    Refactoring Sports: add one loop “for” instead of the three ones

--- Lesson_8/sports/script.js
+++ Lesson_8/sports/script.js
@@ -1,35 +1,45 @@
 const sports = [
   ['skier','⛷'],
-	['snowboarder','🏂'],
-	['apple','🍎'],
-	['hockey','🏒'],
-	['ice skate','⛸'],
-	['swimmer','🏊'],
-	['surfer','🏄‍'],
-	['watermelon','🍉'],
-	['lemon','🍋'],
-	['rowboat','🚣'],
-	['bicyclist','🚴‍']
+  ['snowboarder','🏂'],
+  ['apple','🍎'],
+  ['hockey','🏒'],
+  ['ice skate','⛸'],
+  ['swimmer','🏊'],
+  ['surfer','🏄‍'],
+  ['watermelon','🍉'],
+  ['lemon','🍋'],
+  ['rowboat','🚣'],
+  ['bicyclist','🚴‍']
 ];
 
 const winterSports = sports.slice(0, 5);
 const summerSports = sports.slice(5);
 const fruits = [].concat(winterSports.splice(2, 1), summerSports.splice(2, 2));
 
+let maxLengthArr = winterSports.length;;
+
+if (maxLengthArr < summerSports.length) {
+  maxLengthArr = summerSports.length;
+} else if (maxLengthArr < fruits.length) {
+  maxLengthArr = fruits.length;
+}
+
 let finalWinterSports = '';
 let finalSummerSports = '';
 let finalFruits = '';
 
-for (let i = 0; i < winterSports.length; i++) {
-  finalWinterSports += `${winterSports[i].join(': ')}\n`;
-}
+for (let i = 0; i < maxLengthArr; i++) {
+  if (winterSports[i]) {
+    finalWinterSports += `${winterSports[i].join(': ')}\n`;
+  }
 
-for (let i = 0; i < summerSports.length; i++) {
-  finalSummerSports += `${summerSports[i].join(': ')}\n`;
-}
+  if (summerSports[i]) {
+    finalSummerSports += `${summerSports[i].join(': ')}\n`;
+  }
 
-for (let i = 0; i < fruits.length; i++) {
-  finalFruits += `${fruits[i].join(': ')}\n`;
+  if (fruits[i]) {
+    finalFruits += `${fruits[i].join(': ')}\n`;
+  }
 }
 
 console.log(`*** Winter sports ***
