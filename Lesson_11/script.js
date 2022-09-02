e604f7095f7a0fd9e015c93fe4c2d1955cc01978
laneleka <4ebotaeva@gmail.com>
2022-09-03 02:14:03 +0300

    Added Homework for Lesson 11

---
+++ Lesson_11/script.js
@@ -0,0 +1,28 @@
+const products = [
+	['apple', 10],
+	['banana', 8],
+	['mango', 20],
+	['grape', 18]
+]
+
+function summerValue(value) {
+	return value * 0.8;
+}
+
+function winterValue(value) {
+	return value * 2;
+}
+
+function getPrice(products, seasonFunc) {
+	const copiedProducts = [...products];
+ 
+	let sumProducts = 0;
+ 
+	copiedProducts.forEach(product => sumProducts += seasonFunc &&  typeof seasonFunc === 'function' ? seasonFunc(product[1]) : product[1]);
+	
+	return sumProducts;
+}
+
+console.log(getPrice(products, summerValue));
+console.log(getPrice(products, winterValue));
+console.log(getPrice(products));
